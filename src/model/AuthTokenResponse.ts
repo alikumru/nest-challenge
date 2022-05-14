39e45ad1ca2e207773f6be17068948b3b3ed2713
KUMRUAL <ali.kumru@daimler.com>
2022-05-14 12:36:20 +0300

    first commit

---
+++ src/model/AuthTokenResponse.ts
@@ -0,0 +1,5 @@
+import { Response } from "./Response";
+
+export class Response_AuthToken extends Response {
+     access_token?: string;
+}
---
+++ src/user/authenticate/authenticate.controller.ts
@@ -0,0 +1,44 @@
+import { AuthenticateService } from './authenticate.service';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { AuthenticateUserByPasswordRequestDTO } from './authenticate.dto';
+import { ConfigService } from '@nestjs/config';
+import { Response_AuthToken } from '../../model/AuthTokenResponse';
+
+@Controller('authenticate')
+export class AuthenticateController {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly authenticateService: AuthenticateService,
+  ) {}
+
+  /**
+   * @name AuthenticateUser
+   * @description Authenticate a User with a user and password
+   * @returns boolean
+   */
+  @Post()
+  @HttpCode(200)
+  async AuthenticateUser(
+    @Body() body: AuthenticateUserByPasswordRequestDTO,
+  ): Promise<Response_AuthToken> {
+    const { username, password } = body;
+    try {
+      const token = await this.authenticateService.authenticateUser(
+        username,
+        password,
+      );
+
+      console.log('token: ', token);
+
+      return {
+        access_token: token 
+      };
+    } catch (error) {
+      console.log('AuthenticateUser Error: ', error);
+      return {
+        code: 401,
+        message: error.message
+      };
+    }
+  }
+}
